104b9e6f999ef320a44e9b3c0303dc9cbd97cf9a
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2023-12-28 12:23:59 -0800

    Update filters.py

--- filters.py
+++ filters.py
@@ -90,7 +90,7 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 # private_repository requires URL in relative form
-geo_df = pd.read_excel( Path('./data/GeoReferenceTableBC.xlsx' ) )
+geo_df = pd.read_excel( './data/GeoReferenceTableBC.xlsx' ) 
 df = pd.read_csv(
     "./data/sample_2019_repeated_10yr_weekly.csv" 
 )
